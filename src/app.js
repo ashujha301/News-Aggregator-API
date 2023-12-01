0f574dede815eabca7fd7436eb8a4142cc77760e
Ayush Jha <ashujha301@gmail.com>
2023-12-01 17:31:05 +0530

    Login user , verifytoken middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("./model/user");
+const verifyToken = require('./middeware/verifytoken');
 const app = express();
 require("dotenv").config();
 
@@ -11,6 +12,7 @@ let port = process.env.PORT || 3000;
 //Body parsing middleware
 app.use(express.json());
 
+//-------------------------------------------------------XXXX---------------------------------------------------------------------------------
 //mongoDB connection
 try {
   mongoose.connect("mongodb://127.0.0.1:27017/usersdb", {
@@ -27,33 +29,73 @@ app.get("/", (req, res) => {
   res.status(200).send("News Aggregator API application!!");
 });
 
+//-------------------------------------------------------XXXX---------------------------------------------------------------------------------
 //Register new user
 app.post("/register", async (req, res) => {
-    try {
-      const user = new User({
-        fullname: req.body.fullname,
-        email: req.body.email,
-        role: req.body.role,
-        password: bcrypt.hashSync(req.body.password, 8),
-      });
-  
-      const savedUser = await user.save();
-  
-      return res.status(200).json({ message: "user saved successfully", data: savedUser });
-    } catch (err) {
-      console.error("Error saving user:", err);
-  
-      if (err.code === 11000) {
-        // Duplicate key error (unique index violation), handle accordingly
-        return res.status(400).json({ message: "Email already exists", error: err.message });
-      }
-  
-      return res.status(400).json({ message: "user saving failed", error: err.message });
+  try {
+    const user = new User({
+      fullname: req.body.fullname,
+      email: req.body.email,
+      role: req.body.role,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    const savedUser = await user.save();
+
+    return res
+      .status(200)
+      .json({ message: "user saved successfully", data: savedUser });
+  } catch (err) {
+    console.error("Error saving user:", err);
+
+    if (err.code === 11000) {
+      // Duplicate key error (unique index violation), handle accordingly
+      return res
+        .status(400)
+        .json({ message: "Email already exists", error: err.message });
     }
-  });
 
+    return res
+      .status(400)
+      .json({ message: "user saving failed", error: err.message });
+  }
+});
+//-------------------------------------------------------XXXX---------------------------------------------------------------------------------
 //Login user
+app.post("/login", (req, res) => {
+  try {
+    let emailPassed = req.body.email;
+    let passwordPassed = req.body.password;
 
+    User.findOne({
+      email: emailPassed,
+    }).then((user) => {
+      var passwordIsValid = bcrypt.compareSync(passwordPassed, user.password);
+
+      if (!passwordIsValid) {
+        return res.status(400).send("Invalid passoword");
+      }
+
+      // Create and sign a JWT token for the user
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
+      return res.status(200).json({ message: "Login successful", token });
+    });
+  } catch (err) {
+    console.error("Error during login:", err);
+    return res
+      .status(500)
+      .json({ message: "Login failed", error: err.message });
+  }
+});
+
+//-------------------------------------------------------XXXX---------------------------------------------------------------------------------
+//
 
 app.listen(port, (err) => {
   if (err) {
--- src/middeware/verifytoken.js
+++ src/middeware/verifytoken.js
@@ -3,6 +3,21 @@ require('dotenv').config();
 
 const key = process.env.SECRET_KEY
 
-const verifyToken = (req,res,next) => {
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized: Token missing' });
+    }
+  
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Unauthorized: Invalid token' });
+      }
+  
+      req.user = user;
+      next();
+    });
+  };
 
-}
+  module.exports = verifyToken;
